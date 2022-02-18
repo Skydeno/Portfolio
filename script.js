66cd13b754fc90dbb51ef46c2ada22670b750dce
Skydeno <85034929+Skydeno@users.noreply.github.com>
2022-02-18 05:00:53 +0100

    upgrading script

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-const togglerButton = document.getElementsByClassName('toggler-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const togglerButton = document.getElementsByClassName('toggler-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 togglerButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 });
 
 
-const navbarItem = document.querySelectorAll('.nav-item')[0]
-const navbarItem1 = document.querySelectorAll('.nav-item1')[0]
-const navbarItem2 = document.querySelectorAll('.nav-item2')[0]
+const navbarItem = document.querySelectorAll(".nav-item");
 
-navbarItem.addEventListener('click', () => {
-    if(navbarLinks.classList.contains('active')) {
-        navbarLinks.classList.toggle('active');
-    }
-});
-
-navbarItem1.addEventListener('click', () => {
-    if(navbarLinks.classList.contains('active')) {
-        navbarLinks.classList.toggle('active');
-    }
-});
-
-navbarItem2.addEventListener('click', () => {
-    if(navbarLinks.classList.contains('active')) {
-        navbarLinks.classList.toggle('active');
-    }
+navbarItem.forEach(function(item){
+    item.addEventListener('click', () => {
+        if(navbarLinks.classList.contains('active')) {
+            navbarLinks.classList.toggle('active');
+        }
+    })
 });
